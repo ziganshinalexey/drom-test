f840e4543a2f2bd70c0877891dbd2466c3473516
Alexey Ziganshin <ziga.alexey@gmail.com>
2019-10-19 10:21:53 +0700

    #12345 go home

---
+++ core.php
@@ -0,0 +1,8 @@
+<?php
+
+use Core\Application\Application;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$application = new Application();
+var_dump($application); die;
---
+++ vendor/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+
+var_dump(123342); die;
+spl_autoload_register();
